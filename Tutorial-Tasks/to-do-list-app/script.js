073ccb4304a3d440832d8bc1b4fe7f26c1419e8f
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-07-06 22:32:32 +0545

    basic js logic for to do list app

--- Tutorial-Tasks/to-do-list-app/script.js
+++ Tutorial-Tasks/to-do-list-app/script.js
@@ -1,22 +1,15 @@
 const addBtn = document.querySelector(".add-btn");
 const todoList = document.querySelector(".todos");
 const userInput = document.querySelector(".user-input input");
-const todos = [];
 
 const addTask = () => {
-  let task = {
-    text: userInput.value,
-  };
-  console.log("1: " + task);
-  todos.push(task);
-
-  displayTask(todos);
-};
-
-const displayTask = (task) => {
-  console.log("2" + todos);
-  todoList.innerHTML = todos.map((todo) => {
-    `<li>${todo.text}<i class="fa-solid fa-trash"></i></li>`;
+  let text = userInput.value;
+  if (text.trim() == "") return;
+  let li = document.createElement("li");
+  li.innerHTML = `${text}<i class="fa-solid fa-trash"></i>`;
+  todoList.appendChild(li);
+  li.querySelector("i").addEventListener("click", () => {
+    li.remove();
   });
 };
 
