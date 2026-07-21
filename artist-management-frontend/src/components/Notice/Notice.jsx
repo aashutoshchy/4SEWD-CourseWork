de2075ffc9ddad151ee5a18368f443f490489908
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-07-21 23:25:33 +0545

    Implement Clear Input

--- artist-management-frontend/src/components/Notice/Notice.jsx
+++ artist-management-frontend/src/components/Notice/Notice.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import NoticeCard from "./NoticeCard";
 import "./Notice.css";
 
 function Notice() {
+  const [noticeSearch, setNoticeSearch] = useState();
+
   return (
     <div className="notice">
       <p className="heading">Notice</p>
       <div className="notice-search">
-        <input type="text" placeholder="Search" />
         <i className="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          placeholder="Search"
+          value={noticeSearch}
+          onChange={(e) => setNoticeSearch(e.target.value)}
+        />
+        {noticeSearch && (
+          <i
+            onClick={() => setNoticeSearch("")}
+            className="fa-solid fa-xmark"
+          ></i>
+        )}
       </div>
       <div className="notices">
         <NoticeCard />
